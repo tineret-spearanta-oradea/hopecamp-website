44cb37065a64897a935ea3316fe33c4a013a7b25
Efraim Ghiurau <92709351+ghefraim@users.noreply.github.com>
2022-04-29 21:28:29 +0300

    Add files via upload

--- QRscanner.js
+++ QRscanner.js
@@ -1,8 +1,7 @@
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
 import { getDatabase, ref, child, get, set, update  } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js";
 import { getAuth, onAuthStateChanged, signOut} from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
-import { firebaseConfig } from "./fb_cfg.js";
+import { firebaseConfig } from "../fb_cfg.js";
 
 const adminsURL = "/admins.html";
 
@@ -23,91 +22,113 @@ get(child(dbRef, `users`)).then((snapshot) => {
     console.log(error.message);
 });;
 
-var scanner = new Instascan.Scanner({ video: document.getElementById('preview'), scanPeriod: 5, mirror: false});
-scanner.addListener('scan', function(content){
-    let current_log = document.querySelector("#current-log p");
+qrcode = window.qrcode;
 
-    console.log(content);
-    if(!isNaN(content)){
-        // UI handling
-        current_log.parentElement.classList.add("loading-database");
-        current_log.innerHTML = `Adding to database... ID=${content}`;
-        let user_name="", user_uid="";
+const video = document.createElement("video");
+const canvasElement = document.getElementById("qr-canvas");
+const canvas = canvasElement.getContext("2d");
 
-        // Get user's name by knowing his ID
-        Object.keys(usersData).forEach(uid => {
-            let user = usersData[uid];
-            if(user.qr_id == parseInt(content)) {
-                user_name = user.name;
-                user_uid = uid; 
-            }
-        });
+const qrResult = document.getElementById("qr-result");
+const outputData = document.getElementById("outputData");
+const btnScanQR = document.getElementById("btn-scan-qr");
 
-        // Push prezent to database
-        update(ref(database, `users/${user_uid}`), { prezent:true });
+let scanning = true;
 
-        current_log.parentElement.classList.add("show-result");
-        current_log.innerHTML = `&#x2713; ${user_name} ID=${content}`;
+qrcode.callback = res => {
+    if (res) {
+        // scanning = false;
+        let current_log = document.querySelector("#current-log p");
 
-        let logs = document.querySelector("#logs p");
-        let logs_text = logs.innerHTML;
-        let now = new Date();
-        logs.innerHTML = `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()} - ${user_name} (ID: ${content})<br>${logs_text}`;
+        if(!isNaN(res)){
+            // UI handling
+            current_log.parentElement.classList.add("loading-database");
+            current_log.innerHTML = `Adding to database... ID=${res}`;
+            let user_name="", user_uid="";
 
-        setTimeout(backToReadingUI, 2500);
-        
-    } else {
-        current_log.parentElement.classList.add("loading-database");
-        current_log.innerHTML = `QR CODE NOT VALID`;
+            // Get user's name by knowing his ID
+            Object.keys(usersData).forEach(uid => {
+                let user = usersData[uid];
+                if(user.qr_id == parseInt(res)) {
+                    user_name = user.name;
+                    user_uid = uid; 
+                }
+            });
 
-        setTimeout(backToReadingUI, 2500);
-    }
-});
+            // Push prezent to database
+            update(ref(database, `users/${user_uid}`), { prezent:true });
+
+            document.querySelector("body").style.backgroundColor = "#2d5c3f";
+            current_log.parentElement.classList.add("show-result");
+            current_log.innerHTML = `&#x2713; ${user_name} ID=${res}`;
+
+            let logs = document.querySelector("#logs p");
+            let logs_text = logs.innerHTML;
+            let now = new Date();
+            logs.innerHTML = `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()} - ${user_name} (ID: ${res})<br>${logs_text}`;
+
+            setTimeout(backToReadingUI, 2500);
+            
+        } else {
+            current_log.parentElement.classList.add("loading-database");
+            current_log.innerHTML = `QR CODE NOT VALID`;
+
+            setTimeout(backToReadingUI, 2500);
+        }
+    // outputData.innerText = res;
+    // scanning = false;
+
+    // video.srcObject.getTracks().forEach(track => {
+    //   track.stop();
+    // });
+
+    // qrResult.hidden = false;
+    // canvasElement.hidden = true;
+    // btnScanQR.hidden = false;
+
+
+  }
+};
+
 
 function backToReadingUI(){
+    document.querySelector("body").style.backgroundColor = "#353a36";
     let current_log = document.querySelector("#current-log p");
     current_log.parentElement.classList.remove("show-result");
     current_log.parentElement.classList.remove("loading-database");
     current_log.innerHTML = "Reading...";
+    // scanning = true;
 
 }
 
-Instascan.Camera.getCameras().then(function (cameras){
-    if(cameras.length>0){
-        scanner.start(cameras[0]);
-        $('[name="options"]').on('change',function(){
-            if($(this).val()==1){
-                if(cameras[0]!=""){
-                    scanner.start(cameras[0]);
-                }else{
-                    alert('No Front camera found!');
-                }
-            }else if($(this).val()==2){
-                if(cameras[1]!=""){
-                    scanner.start(cameras[1]);
-                }else{
-                    alert('No Back camera found!');
-                }
-            }
-        });
-    }else{
-        console.error('No cameras found.');
-        alert('No cameras found.');
-    }
-}).catch(function(e){
-    console.error(e);
-    alert(e);
-});
 
+// btnScanQR.onclick = () => {
+navigator.mediaDevices
+    .getUserMedia({ video: { facingMode: "environment" } })
+    .then(function(stream) {
+    //   scanning = true;
+        qrResult.hidden = true;
+        btnScanQR.hidden = true;
+        canvasElement.hidden = false;
+        video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
+        video.srcObject = stream;
+        video.play();
+        tick();
+        scan();
+    });
+// };
 
+function tick() {
+  canvasElement.height = video.videoHeight;
+  canvasElement.width = video.videoWidth;
+  canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
 
-function countProperties(obj) {
-  var count = 0;
+  scanning &&  requestAnimationFrame(tick);
+}
 
-  for(var prop in obj) {
-    if(obj.hasOwnProperty(prop))
-      ++count;
+function scan() {
+  try {
+    qrcode.decode();
+  } catch (e) {
+    setTimeout(scan, 300);
   }
-
-  return count;
 }
