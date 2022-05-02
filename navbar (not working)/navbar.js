bdb331ef9ea154f36c4e33fbb1ebec03339f611a
Efraim Ghiurau <92709351+ghefraim@users.noreply.github.com>
2022-05-02 19:32:36 +0300

    Add files via upload

---
+++ navbar (not working)/navbar.js
@@ -0,0 +1,15 @@
+// STICKY NAVBAR FROM W3S
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
