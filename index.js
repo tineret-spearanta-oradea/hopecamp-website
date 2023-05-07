fe41a02c69aade9c7496101e4cd545587a97db92
Efraim Ghiurau <92709351+ghefraim@users.noreply.github.com>
2023-05-07 16:51:39 +0000

    more updates

--- index.js
+++ index.js
@@ -27,5 +27,5 @@ function showSlides() {
     slideIndex++;
     if (slideIndex > slides.length) { slideIndex = 1 }
     slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 4000); // Change image every 2 seconds
+    setTimeout(showSlides, 2700); // Change image every 3 seconds
 }
---
+++ readme.md
@@ -0,0 +1,3 @@
+# HopeCamp
+This is the codebase for hopecamp.ro website.
+The website represents the christian youth camp, and has sign-up functionalities
