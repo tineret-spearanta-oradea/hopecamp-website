4ee65b881de790739dd09b4b7aa40777308d50c0
EfraimGhiurau <efy009@gmail.com>
2023-05-07 16:48:30 +0300

    another update

--- index.js
+++ index.js
@@ -13,3 +13,19 @@ const navbtn = document.querySelector(".burger-menu").addEventListener("click", 
         // navigation.style.backgroundColor = "rgba(0, 0, 0, 0)";
     }
 });
+
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
