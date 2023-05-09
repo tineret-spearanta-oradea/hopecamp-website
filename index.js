d5caa4ce27e71160144fd5a743637056b04e4565
EfraimGhiurau <efy009@gmail.com>
2023-05-09 17:34:48 +0300

    added faqs

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ const navbtn = document.querySelector(".burger-menu").addEventListener("click", 
 });
 
 
+// IMAGE SLIDER
 let slideIndex = 0;
 showSlides();
-
 function showSlides() {
     let i;
     let slides = document.getElementsByClassName("mySlides");
@@ -28,4 +28,20 @@ function showSlides() {
     if (slideIndex > slides.length) { slideIndex = 1 }
     slides[slideIndex - 1].style.display = "block";
     setTimeout(showSlides, 2700); // Change image every 3 seconds
+}
+
+
+// ACCORDION FAQ
+var acc = document.getElementsByClassName("accordion");
+var i;
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
 }
