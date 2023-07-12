bb42b9fa38b4e2177ede4c11ee3cc7b70b81f7b3
Efraim Ghiurau <92709351+ghefraim@users.noreply.github.com>
2023-07-12 10:42:31 +0000

    remove signup possibility

--- signup.js
+++ signup.js
@@ -28,18 +28,18 @@ const pfpRef = storage_ref(storage, "profilepics");
 let isMobile = false;
 
 // COMMENT THESE NEXT YEAR :) if we use this site*****************
-// document.querySelector("#changeable-content-1").innerHTML = '';
-// document.querySelector("#changeable-content-0").innerHTML =
-// `
-//  <div id="card-subtitle" style="height: 160px">
-//   <p>
-//     Ne pare rău.<br>
-//     <strong>Locurile din tabără au fost ocupate!</strong><br>
-//     Vestea bună e că ne poți vizita pe timp de zi
-//     29 iulie - 3 august, Vila Speranța, Arieșeni.
-//   </p>
-//  </div>
-// `;
+document.querySelector("#changeable-content-1").innerHTML = '';
+document.querySelector("#changeable-content-0").innerHTML =
+`
+ <div id="card-subtitle" style="height: 160px">
+  <p>
+    Ne pare rău.<br>
+    <strong>Locurile din tabără au fost ocupate!</strong><br>
+    Vestea bună e că ne poți vizita pe timp de zi
+    29 iulie - 3 august, Vila Speranța, Arieșeni.
+  </p>
+ </div>
+`;
 //***************************************************************
 
 
