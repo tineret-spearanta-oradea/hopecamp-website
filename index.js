199acf5b7762132a63e80f03735ce912afb83d6a
George04060 <104093534+George04060@users.noreply.github.com>
2023-07-26 11:39:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -126,22 +126,7 @@ function searchFunction() {
     }, 0);
 })();
 
-// $(function() {
-// $('img[data-enlargable]').addClass('img-enlargable').click(function(){
-//     var src = $(this).attr('src');
-//     $('<div>').css({
-//         background: 'RGBA(0,0,0,.5) url('+src+') no-repeat center',
-//         backgroundSize: 'contain',
-//         width:'100%', height:'100%',
-//         position:'fixed',
-//         zIndex:'10000',
-//         top:'0', left:'0',
-//         cursor: 'zoom-out'
-//     }).click(function(){
-//         $(this).remove();
-//     }).appendTo('body');
-// });
-// })
+
 
 
 
