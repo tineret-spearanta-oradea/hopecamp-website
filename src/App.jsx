28f969b6ef2b29bd3c007ba90de285e40295048a
DenisWithOneN <denis.ursut@emanuel.ro>
2024-04-04 16:34:06 +0300

    initialized react + vite setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <div className="flex justify-center items-center min-h-screen">
+        <p className="text-2xl">Welcome HopeCamp</p>
+      </div>
+    </>
+  )
+}
+
+export default App
