4a795d504f72ff6af71cf0583fa14195648df607
DenisWithOneN <denis.ursut@emanuel.ro>
2024-04-04 18:45:05 +0300

    initialization react + vite project

---
+++ hopecamp-website/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <div className='text-4xl flex justify-center items-center min-h-screen'>Welcome HopeCamp</div>
+    </>
+  )
+}
+
+export default App
