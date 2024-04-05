ee979fcd7f8c743ea9e014bf10a056e5ba7d4fb2
Francis <65733799+MaxiFrancis@users.noreply.github.com>
2024-04-05 22:21:21 +0300

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDwUUICAR38G8M43QBSnCegzSkbYUGrQTU",
-  authDomain: "hopecamp-69aa4.firebaseapp.com",
-  projectId: "hopecamp-69aa4",
-  storageBucket: "hopecamp-69aa4.appspot.com",
-  messagingSenderId: "854377150009",
-  appId: "1:854377150009:web:bcb2e26a3e5f202d59bfe8",
-  measurementId: "G-JR0TC62DPH"
+  apiKey: "your apiKey",
+  authDomain: "your authDomain",
+  projectId: "your projectId",
+  storageBucket: "your storageBucket",
+  messagingSenderId: "your messagingSenderId",
+  appId: "your appId",
+  measurementId: "your measurementId"
 };
 
 
