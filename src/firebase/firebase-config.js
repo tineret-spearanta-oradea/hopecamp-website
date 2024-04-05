8dabcd8c466ae61883c68cd7b4ee41cb14f5081c
Francis <65733799+MaxiFrancis@users.noreply.github.com>
2024-04-06 02:17:32 +0300

    Firebase Authentication Logic
    Not ready

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -2,17 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "your apiKey",
-  authDomain: "your authDomain",
-  projectId: "your projectId",
-  storageBucket: "your storageBucket",
-  messagingSenderId: "your messagingSenderId",
-  appId: "your appId",
-  measurementId: "your measurementId"
+  apiKey: "AIzaSyDwUUICAR38G8M43QBSnCegzSkbYUGrQTU",
+  authDomain: "hopecamp-69aa4.firebaseapp.com",
+  projectId: "hopecamp-69aa4",
+  storageBucket: "hopecamp-69aa4.appspot.com",
+  messagingSenderId: "854377150009",
+  appId: "1:854377150009:web:bcb2e26a3e5f202d59bfe8",
+  measurementId: "G-JR0TC62DPH",
 };
 
-
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app)
+const auth = getAuth(app);
 
-export { app, auth }
+export { app, auth };
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -8,11 +8,10 @@ const firebaseConfig = {
   storageBucket: "hopecamp-69aa4.appspot.com",
   messagingSenderId: "854377150009",
   appId: "1:854377150009:web:bcb2e26a3e5f202d59bfe8",
-  measurementId: "G-JR0TC62DPH"
+  measurementId: "G-JR0TC62DPH",
 };
 
-
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app)
+const auth = getAuth(app);
 
-export { app, auth }
+export { app, auth };
