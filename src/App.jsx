c0b43aa5afc670b5218eb02f627faa97a5d6bc68
ghefraim <efy009@gmail.com>
2024-04-06 16:12:03 +0300

    Add Tailwind and message on index

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { useAuth } from "./contexts/authContext"; // Adjust path if needed
 import Login from "./components/auth/login"; // Adjust path if needed
 import Register from "./components/auth/register"; // Adjust path if needed
 import Home from "./components/home"; // Adjust path if needed
-
+import './index.css';
 function App() {
   const { UserLoggedIn } = useAuth();
 
--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
-import { useAuth } from '../../contexts/authContext'
 
 const Home = () => {
-    const { currentUser } = useAuth()
     return (
-        <div className='text-2xl font-bold pt-14'>Hello {currentUser.displayName ? currentUser.displayName : currentUser.email}, you are now logged in.</div>
+        <div className='pt-20'>
+            <div>
+                <div className='text-4xl font-bold flex justify-center'>
+                    404 Page not found
+                </div>
+                <div className='text-base flex justify-center '>
+                    Work in progress... Please, come back later!
+                </div>
+            </div>
+        </div>
     )
 }
 
-export default Home
+export default Home 
