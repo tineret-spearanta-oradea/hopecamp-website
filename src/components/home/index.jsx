2619034257be1fece6df8a1a2f5547826cbcad7c
ghefraim <efy009@gmail.com>
2024-04-06 16:12:03 +0300

    Add Tailwind and message on index

---
+++ src/components/home/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className='pt-20'>
+            <div>
+                <div className='text-4xl font-bold flex justify-center'>
+                    404 Page not found
+                </div>
+                <div className='text-base flex justify-center '>
+                    Work in progress... Please, come back later!
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/**/*.{js,jsx,ts,tsx,html}",
+    "./src/**/*.{js,jsx,ts,tsx,html}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
