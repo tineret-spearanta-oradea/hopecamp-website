859b78cf401a02009f688105ae2bc0780a95ecd7
Francis <65733799+MaxiFrancis@users.noreply.github.com>
2024-04-13 01:54:21 +0300

    Addning styles

--- src/components/navigationBar/index.jsx
+++ src/components/navigationBar/index.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const NavigationBar = () => {
   return (
-    <nav className="bg-blue-500 text-white p-4 shadow-md">
+    <nav className="bg-blue-500 text-white p-2 shadow-md">
       <div className="container mx-auto flex justify-between items-center">
         <Link to="/" className="text-lg font-semibold hover:text-blue-200 transition-colors py-2 px-4 rounded">Home</Link>
         <div className="flex space-x-4">
