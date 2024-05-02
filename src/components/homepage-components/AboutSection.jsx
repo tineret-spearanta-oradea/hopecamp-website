1e2f6be93f4a66c47d9902dcf19f297124b0c586
DenisWithOneN <denis.ursut@emanuel.ro>
2024-05-02 13:54:36 +0300

    integrated About section and skeleton with design

---
+++ src/components/homepage-components/AboutSection.jsx
@@ -0,0 +1,20 @@
+function AboutSection() {
+  return (
+    <>
+      <section className="px-5">
+        <div className="container mx-auto flex flex-col items-center gap-5">
+          <h2 className="text-black text-3xl font-bold sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl">Despre noi</h2>
+          <p className="text-center font-medium">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quod
+            porro sapiente doloremque, beatae necessitatibus libero. Debitis
+            error atque iste alias, distinctio doloribus magni, doloremque, fuga
+            odit odio quam vero?
+          </p>
+          <img src="src/assets/images/ZVE03429.jpg" alt="group-photo-ZVE03429" />
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default AboutSection;
