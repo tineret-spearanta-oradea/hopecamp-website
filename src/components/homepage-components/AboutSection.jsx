9c0ad2a8b5340fd9238ef3bb571fd88d2e612ce5
DenisWithOneN <denis.ursut@emanuel.ro>
2024-05-02 14:31:29 +0300

    changed the svg file with a modified version to solve the huge gap between sections

--- src/components/homepage-components/AboutSection.jsx
+++ src/components/homepage-components/AboutSection.jsx
@@ -3,17 +3,17 @@ function AboutSection() {
     <>
       <section className="px-5">
         <div className="container mx-auto flex flex-col items-center gap-5">
-          <h2 className="text-black text-3xl font-bold sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl">
+          <h2 className="text-black text-3xl font-bold sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl">
             Despre noi
           </h2>
-          <p className="text-center font-medium">
+          <p className="text-center font-medium max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quod
             porro sapiente doloremque, beatae necessitatibus libero. Debitis
             error atque iste alias, distinctio doloribus magni, doloremque, fuga
             odit odio quam vero?
           </p>
           <div
-            className="h-32 w-full"
+            className="h-32 w-full max-w-xl rounded-lg"
             style={{
               backgroundImage: "url('/src/assets/images/ZVE03429.jpg')",
               backgroundPosition: "center",
