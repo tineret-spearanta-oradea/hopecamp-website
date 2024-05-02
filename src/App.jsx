ab9af14e3270ed7c023b66163766930598224475
DenisWithOneN <denis.ursut@emanuel.ro>
2024-05-02 13:02:44 +0300

    integrated first component into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
+import HomePage from "./pages/HomePage"
+
 function App() {
 
   return (
     <>
-      <div className="flex justify-center items-center min-h-screen">
-        <p className="text-2xl">Welcome HopeCamp</p>
-      </div>
+      <HomePage/>
     </>
   )
 }
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+function HomePage() {
+    return(
+        <>
+        <div>test</div>
+        
+        </>
+    );
+}
+
+export default HomePage
