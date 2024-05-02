84f3e2012c45c2e24d4f69d42a831b9548187093
DenisWithOneN <denis.ursut@emanuel.ro>
2024-05-02 15:06:59 +0300

    added SignUpSection component with sign up button and svgs

---
+++ src/components/homepage-components/SignUpSection.jsx
@@ -0,0 +1,16 @@
+import FilledButton from "../general-components/FilledButton";
+import '/src/styles/dividers.css'
+
+function SignUpSection() {
+    return <>
+    <section>
+        <div className="spacer button-divider-up"/>
+        <div className='bg-hope-beige h-24 flex justify-center items-center'>
+        <FilledButton text="Înscrie-te"></FilledButton>
+        </div>
+        <div className="spacer button-divider-down"/>
+    </section>
+    </>
+}
+
+export default SignUpSection
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import HeroSection from "../components/homepage-components/HeroSection";
 import AboutSection from "../components/homepage-components/AboutSection";
+import SignUpSection from "../components/homepage-components/SignUpSection";
 
 function HomePage() {
     return(
         <>
         <HeroSection/>
         <AboutSection/>
+        <SignUpSection/>
         
         </>
     );
