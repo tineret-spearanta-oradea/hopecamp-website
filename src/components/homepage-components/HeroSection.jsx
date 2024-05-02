11013c249a0c69ad7db3b328b18b9137cd8415d1
DenisWithOneN <denis.ursut@emanuel.ro>
2024-05-02 14:41:35 +0300

    changed section content padding

--- src/components/homepage-components/HeroSection.jsx
+++ src/components/homepage-components/HeroSection.jsx
@@ -4,7 +4,7 @@ import '/src/styles/dividers.css'
 function HeroSection() {
   return (
     <>
-      <section className="bg-hope-beige py-14 px-5">
+      <section className="bg-hope-beige py-14 px-8">
         <div className="container mx-auto flex flex-col justify-center items-center gap-5">
           <h1 className="text-hope-darkcyan text-3xl font-black sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl">HopeCamp</h1>
           <p className="text-black text-center text-sm font-semibold max-w-44 sm:text-sm md:text-md lg:text-lg lg:max-w-lg xl:text-xl 2xl:text-2xl">
