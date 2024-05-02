ebff9e38cd4e82dae070c7f85105583b4af390ce
DenisWithOneN <denis.ursut@emanuel.ro>
2024-05-02 13:37:30 +0300

    added section divider using Haikei.app

--- src/components/homepage-components/HeroSection.jsx
+++ src/components/homepage-components/HeroSection.jsx
@@ -1,13 +1,19 @@
+import FilledButton from "../general-components/FilledButton";
+import '/src/styles/dividers.css'
+
 function HeroSection() {
   return (
     <>
-      <section>
-        <div className="flex flex-col justify-center items-center gap-5">
-          <h1>HopeCamp</h1>
-          <p>The one camp you don&apos;t want to miss</p>
-          <div>button</div>
+      <section className="bg-hope-beige py-14 px-10">
+        <div className="container mx-auto flex flex-col justify-center items-center gap-5">
+          <h1 className="text-hope-darkcyan text-3xl font-black">HopeCamp</h1>
+          <p className="text-black text-center font-semibold max-w-52">
+            The one camp you don&apos;t want to miss
+          </p>
+          <FilledButton text="Înscrie-te" />
         </div>
       </section>
+      <div className="spacer hero-divider"></div>
     </>
   );
 }
