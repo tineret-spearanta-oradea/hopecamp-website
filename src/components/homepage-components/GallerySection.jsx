1f7936880f39dac5b58de2ab5bba9b5496faa7dd
DenisWithOneN <denis.ursut@emanuel.ro>
2024-05-02 15:55:42 +0300

    added button to gallery section

--- src/components/homepage-components/GallerySection.jsx
+++ src/components/homepage-components/GallerySection.jsx
@@ -1,3 +1,5 @@
+import FilledButton from "../general-components/FilledButton";
+
 function GallerySection() {
   return (
     <>
@@ -7,18 +9,18 @@ function GallerySection() {
             Galerie
           </h2>
           <div className="grid grid-cols-2 gap-3">
-  <div className="flex flex-col gap-3">
-    <div className="bg-gray-500 w-32 h-24 sm:w-44 sm:h-32 md:w-48 md:h-36 lg:w-56 lg:h-40 xl:w-64 xl:h-48 2xl:w-72 2xl:h-52 bg-cover bg-center rounded-lg"></div>
-    <div className="bg-gray-500 w-32 h-52 sm:w-44 sm:h-60 md:w-48 md:h-64 lg:w-56 lg:h-72 xl:w-64 xl:h-80 2xl:w-72 2xl:h-88 bg-cover bg-center rounded-lg"></div>
-    <div className="bg-gray-500 w-32 h-32 sm:w-44 sm:h-40 md:w-48 md:h-48 lg:w-56 lg:h-64 xl:w-64 xl:h-72 2xl:w-72 2xl:h-80 bg-cover bg-center rounded-lg"></div>
-  </div>
-  <div className="flex flex-col gap-3">
-    <div className="bg-gray-500 w-32 h-44 sm:w-44 sm:h-52 md:w-48 md:h-56 lg:w-56 lg:h-64 xl:w-64 xl:h-72 2xl:w-72 2xl:h-80 bg-cover bg-center rounded-lg"></div>
-    <div className="bg-gray-500 w-32 h-20 sm:w-44 sm:h-24 md:w-48 md:h-28 lg:w-56 lg:h-32 xl:w-64 xl:h-36 2xl:w-72 2xl:h-40 bg-cover bg-center rounded-lg"></div>
-    <div className="bg-gray-500 w-32 h-44 sm:w-44 sm:h-52 md:w-48 md:h-64 lg:w-56 lg:h-80 xl:w-64 xl:h-88 2xl:w-72 2xl:h-80 bg-cover bg-center rounded-lg"></div>
-  </div>
-</div>
-
+            <div className="flex flex-col gap-3">
+              <div className="bg-gray-500 w-32 h-24 sm:w-44 sm:h-32 md:w-48 md:h-36 lg:w-56 lg:h-40 xl:w-64 xl:h-48 2xl:w-72 2xl:h-52 bg-cover bg-center rounded-lg"></div>
+              <div className="bg-gray-500 w-32 h-52 sm:w-44 sm:h-60 md:w-48 md:h-64 lg:w-56 lg:h-72 xl:w-64 xl:h-80 2xl:w-72 2xl:h-88 bg-cover bg-center rounded-lg"></div>
+              <div className="bg-gray-500 w-32 h-32 sm:w-44 sm:h-40 md:w-48 md:h-48 lg:w-56 lg:h-64 xl:w-64 xl:h-72 2xl:w-72 2xl:h-80 bg-cover bg-center rounded-lg"></div>
+            </div>
+            <div className="flex flex-col gap-3">
+              <div className="bg-gray-500 w-32 h-44 sm:w-44 sm:h-52 md:w-48 md:h-56 lg:w-56 lg:h-64 xl:w-64 xl:h-72 2xl:w-72 2xl:h-80 bg-cover bg-center rounded-lg"></div>
+              <div className="bg-gray-500 w-32 h-20 sm:w-44 sm:h-24 md:w-48 md:h-28 lg:w-56 lg:h-32 xl:w-64 xl:h-36 2xl:w-72 2xl:h-40 bg-cover bg-center rounded-lg"></div>
+              <div className="bg-gray-500 w-32 h-44 sm:w-44 sm:h-52 md:w-48 md:h-64 lg:w-56 lg:h-80 xl:w-64 xl:h-88 2xl:w-72 2xl:h-80 bg-cover bg-center rounded-lg"></div>
+            </div>
+          </div>
+          <FilledButton text="Vezi galerie"/>
         </div>
       </section>
     </>
