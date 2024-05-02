5f37174046d08a17e1773d3eb4d1f8a142812b55
DenisWithOneN <denis.ursut@emanuel.ro>
2024-05-02 13:17:04 +0300

    added custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'hope-orange': '#F58726',
+        'hope-lightcyan': '#31A595',
+        'hope-darkcyan': '#0E7E6F',
+        'hope-lightgray': '#D9D9D9',
+        'hope-darkgray': '#5F5F5F',
+      }
+    },
   },
   plugins: [],
 }
