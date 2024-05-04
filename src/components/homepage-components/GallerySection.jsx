7a8a02bae400f50eb60ad8ad66053e6eb1078965
DenisWithOneN <denis.ursut@emanuel.ro>
2024-05-04 23:48:38 +0300

    added photo routes and applied new styles to section

--- src/components/homepage-components/GallerySection.jsx
+++ src/components/homepage-components/GallerySection.jsx
@@ -1,4 +1,5 @@
 import FilledButton from "../general-components/FilledButton";
+import "/src/styles/gallery-routes.css";
 
 function GallerySection() {
   return (
@@ -10,17 +11,17 @@ function GallerySection() {
           </h2>
           <div className="grid grid-cols-2 gap-3">
             <div className="flex flex-col gap-3">
-              <div className="bg-gray-500 w-32 h-24 sm:w-44 sm:h-32 md:w-48 md:h-36 lg:w-56 lg:h-40 xl:w-64 xl:h-48 2xl:w-72 2xl:h-52 bg-cover bg-center rounded-lg"></div>
-              <div className="bg-gray-500 w-32 h-52 sm:w-44 sm:h-60 md:w-48 md:h-64 lg:w-56 lg:h-72 xl:w-64 xl:h-80 2xl:w-72 2xl:h-88 bg-cover bg-center rounded-lg"></div>
-              <div className="bg-gray-500 w-32 h-32 sm:w-44 sm:h-40 md:w-48 md:h-48 lg:w-56 lg:h-64 xl:w-64 xl:h-72 2xl:w-72 2xl:h-80 bg-cover bg-center rounded-lg"></div>
+              <div className="photo1 w-32 h-24 sm:w-44 sm:h-32 md:w-48 md:h-36 lg:w-56 lg:h-40 xl:w-64 xl:h-48 2xl:w-72 2xl:h-52 bg-cover bg-center rounded-lg"></div>
+              <div className="photo2 w-32 h-52 sm:w-44 sm:h-60 md:w-48 md:h-64 lg:w-56 lg:h-72 xl:w-64 xl:h-80 2xl:w-72 2xl:h-88 bg-cover bg-center rounded-lg"></div>
+              <div className="photo3 w-32 h-32 sm:w-44 sm:h-40 md:w-48 md:h-48 lg:w-56 lg:h-64 xl:w-64 xl:h-72 2xl:w-72 2xl:h-80 bg-cover bg-center rounded-lg"></div>
             </div>
             <div className="flex flex-col gap-3">
-              <div className="bg-gray-500 w-32 h-44 sm:w-44 sm:h-52 md:w-48 md:h-56 lg:w-56 lg:h-64 xl:w-64 xl:h-72 2xl:w-72 2xl:h-80 bg-cover bg-center rounded-lg"></div>
-              <div className="bg-gray-500 w-32 h-20 sm:w-44 sm:h-24 md:w-48 md:h-28 lg:w-56 lg:h-32 xl:w-64 xl:h-36 2xl:w-72 2xl:h-40 bg-cover bg-center rounded-lg"></div>
-              <div className="bg-gray-500 w-32 h-44 sm:w-44 sm:h-52 md:w-48 md:h-64 lg:w-56 lg:h-80 xl:w-64 xl:h-88 2xl:w-72 2xl:h-80 bg-cover bg-center rounded-lg"></div>
+              <div className="photo4 w-32 h-44 sm:w-44 sm:h-52 md:w-48 md:h-56 lg:w-56 lg:h-64 xl:w-64 xl:h-72 2xl:w-72 2xl:h-80 bg-cover bg-center rounded-lg"></div>
+              <div className="photo5 w-32 h-20 sm:w-44 sm:h-24 md:w-48 md:h-28 lg:w-56 lg:h-32 xl:w-64 xl:h-36 2xl:w-72 2xl:h-40 bg-cover bg-center rounded-lg"></div>
+              <div className="photo6 w-32 h-44 sm:w-44 sm:h-52 md:w-48 md:h-64 lg:w-56 lg:h-80 xl:w-64 xl:h-88 2xl:w-72 2xl:h-80 bg-cover bg-center rounded-lg"></div>
             </div>
           </div>
-          <FilledButton text="Vezi galerie" route="/galerie"/>
+          <FilledButton text="Vezi galerie" route="/galerie" />
         </div>
       </section>
     </>
