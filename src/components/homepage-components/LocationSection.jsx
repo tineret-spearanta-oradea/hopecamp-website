0f4ca0363264d6f0808a6cc6a68e02c2b7a156e5
DenisWithOneN <denis.ursut@emanuel.ro>
2024-05-05 01:00:00 +0300

    changed map size

--- src/components/homepage-components/LocationSection.jsx
+++ src/components/homepage-components/LocationSection.jsx
@@ -22,7 +22,7 @@ function LocationSection() {
               </p>
             </div>
             <iframe
-              className="w-60 h-60 sm:w-64 sm:h-64 md:w-72 md:h-72 lg:w-80 lg:h-80 xl:w-96 xl:h-96"
+              className="w-60 h-60 sm:w-64 sm:h-64 md:w-72 md:h-72 lg:w-80 lg:h-80 xl:w-100 xl:h-100"
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2736.6649349845015!2d23.311087599999997!3d46.692613699999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4749196164acd61d%3A0x8b3cca28a1e12774!2sComplex%20Turistic%20Puiu!5e0!3m2!1sro!2sro!4v1714846979815!5m2!1sro!2sro"
             ></iframe>
           </div>
