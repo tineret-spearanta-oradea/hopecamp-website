099d7e71ed21120484bd94f8caa59e68c9cd6dfe
DenisWithOneN <denis.ursut@emanuel.ro>
2024-05-04 23:12:16 +0300

    added Link functionality

--- src/components/general-components/FilledButton.jsx
+++ src/components/general-components/FilledButton.jsx
@@ -1,14 +1,22 @@
-import PropTypes from 'prop-types';
-
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 FilledButton.propTypes = {
   text: PropTypes.string.isRequired,
+  route: PropTypes.string.isRequired,
 };
 
 function FilledButton(props) {
-    return <>
-        <button className='bg-hope-orange font-semibold rounded-full px-8 py-2'>{props.text}</button>
+  return (
+    <>
+      <Link
+        className="bg-hope-orange font-semibold rounded-full px-8 py-2"
+        to={props.route}
+      >
+        {props.text}
+      </Link>
     </>
+  );
 }
 
-export default FilledButton
+export default FilledButton;
