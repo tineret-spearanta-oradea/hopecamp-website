4edf1f1024751475618036f931fa8d80fbb4325b
DenisWithOneN <denis.ursut@emanuel.ro>
2024-05-04 23:31:15 +0300

    updated design, added temporary non functional countdown

--- src/components/homepage-components/HeroSection.jsx
+++ src/components/homepage-components/HeroSection.jsx
@@ -1,16 +1,23 @@
 import FilledButton from "../general-components/FilledButton";
-import '/src/styles/dividers.css'
+import "/src/styles/dividers.css";
 
 function HeroSection() {
   return (
     <>
       <section className="bg-hope-beige py-14 px-8">
-        <div className="container mx-auto flex flex-col justify-center items-center gap-5">
-          <h1 className="text-hope-darkcyan text-3xl font-black sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl">HopeCamp</h1>
-          <p className="text-black text-center text-sm font-semibold max-w-44 sm:text-sm md:text-md lg:text-lg lg:max-w-lg xl:text-xl 2xl:text-2xl">
-            The one camp you don&apos;t want to miss
+        <div className="container mx-auto flex flex-col justify-center items-center gap-2 lg:gap-4">
+          <h1 className="text-hope-darkcyan text-3xl font-black sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl">
+            HopeCamp
+          </h1>
+          <p className="text-black text-center text-sm font-medium max-w-44 sm:text-base  lg:text-lg lg:max-w-lg xl:text-xl 2xl:text-2xl">
+            Tabăra pe care nu vrei să o ratezi
           </p>
-          <FilledButton text="Înscrie-te" route="/login" />
+          <p className="text-hope-lightcyan font-black text-3xl lg:text-5xl xl:text-7xl">
+            00:00:00
+          </p>
+          <div className="pt-10">
+            <FilledButton text="Înscrie-te" route="/login" />
+          </div>
         </div>
       </section>
       <div className="spacer hero-divider"></div>
