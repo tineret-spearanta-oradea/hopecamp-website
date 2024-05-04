e2b8d58345a13473728329fa7b1f8e810462a559
DenisWithOneN <denis.ursut@emanuel.ro>
2024-05-04 21:25:07 +0300

    changed map src to display new location

--- src/components/homepage-components/LocationSection.jsx
+++ src/components/homepage-components/LocationSection.jsx
@@ -21,7 +21,7 @@ function LocationSection() {
             </div>
             <iframe
               className="w-60 h-60 sm:w-64 sm:h-64 md:w-72 md:h-72 lg:w-80 lg:h-80 xl:w-96 xl:h-96"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2716.698588278047!2d21.92494277665897!3d47.08537407114672!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47464867544e469b%3A0x4c81f617c162bed1!2sBiserica%20Penticostala%20Speran%C8%9Ba!5e0!3m2!1sro!2sro!4v1714657201589!5m2!1sro!2sro"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2736.6649349845015!2d23.311087599999997!3d46.692613699999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4749196164acd61d%3A0x8b3cca28a1e12774!2sComplex%20Turistic%20Puiu!5e0!3m2!1sro!2sro!4v1714846979815!5m2!1sro!2sro"
             ></iframe>
           </div>
         </div>
