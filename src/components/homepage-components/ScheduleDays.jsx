81f18d5dcca0fde3b88828c6852cf9bd8ec0d6e0
DenisWithOneN <denis.ursut@emanuel.ro>
2024-05-05 00:50:13 +0300

    added open box functionality

--- src/components/homepage-components/ScheduleDays.jsx
+++ src/components/homepage-components/ScheduleDays.jsx
@@ -1,4 +1,5 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 ScheduleDays.propTypes = {
   day: PropTypes.string.isRequired,
@@ -6,11 +7,27 @@ ScheduleDays.propTypes = {
 };
 
 function ScheduleDays(props) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [relativeHeight, setRelativeHeight] = useState("h-14");
+  const [relativeJustify, setrelativeJustify] = useState("justify-center")
+
+  const toggleOpen = () => {
+    setIsOpen(!isOpen);
+    setRelativeHeight(isOpen ? "h-14" : "h-52");
+    setrelativeJustify(isOpen ? "justify-center" : "justify-around")
+  };
+
   return (
     <>
-      <div className="bg-hope-darkcyan w-full h-14 rounded-lg flex items-center justify-between px-3">
-        <p className="text-white font-bold opacity-50">{props.day}</p>
-        <p className="text-white text-sm opacity-50">{props.date}</p>
+      <div
+        onClick={toggleOpen}
+        className={`bg-hope-darkcyan w-full ${relativeHeight} rounded-lg flex flex-col ${relativeJustify} px-3 py-2`}
+      >
+        <div className="flex justify-between">
+          <p className="text-white font-bold opacity-50">{props.day}</p>
+          <p className="text-white text-sm opacity-50">{props.date}</p>
+        </div>
+        {isOpen && <div className="bg-hope-orange w-full h-32 rounded-lg"></div>}
       </div>
     </>
   );
