774167a8f879b18aaaa7e95289b4cb8cb2b3644b
ghefraim <efy009@gmail.com>
2024-05-06 11:55:15 +0300

    We shouldn't have public in the URL path of the static asset from public folder

--- src/components/auth/FormCard.jsx
+++ src/components/auth/FormCard.jsx
@@ -4,7 +4,7 @@ const FormCard = ({ children }) => {
   return (
     <div
       className="bg-scroll bg-cover bg-center bg-no-repeat w-full h-screen flex place-content-center place-items-center"
-      style={{ backgroundImage: 'url("public/assets/images/bg-auth.png")' }}
+      style={{ backgroundImage: 'url("assets/images/bg-auth.png")' }}
     >
       <main className="w-full h-screen flex self-center place-content-center place-items-center">
         <div className="w-96 bg-white bg-opacity-90 text-gray-600 space-y-5 p-6 sm:p-8 shadow-xl border rounded-xl  ">
