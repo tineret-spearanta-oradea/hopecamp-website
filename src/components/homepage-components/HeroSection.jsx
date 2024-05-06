7f9e0f89350dfc66bea3d9cb20a17e8d89118e78
DenisWithOneN <denis.ursut@emanuel.ro>
2024-05-06 22:44:28 +0300

    added countdown

--- src/components/homepage-components/HeroSection.jsx
+++ src/components/homepage-components/HeroSection.jsx
@@ -1,8 +1,36 @@
+import  { useState, useEffect } from "react";
 import FilledButton from "../general-components/FilledButton";
 import "/src/styles/dividers.css";
 import { CampTitle, pages } from "../../constants";
 
 function HeroSection() {
+  const calculateCountdown = () => {
+    const currentDate = new Date();
+    const targetDate = new Date("July 20, 2024");
+    const difference = targetDate - currentDate;
+
+    if (difference <= 0) {
+      return "Hope Camp in progress!";
+    }
+
+    const seconds = Math.floor((difference / 1000) % 60);
+    const minutes = Math.floor((difference / 1000 / 60) % 60);
+    const hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+
+    return `${days}d ${hours}h ${minutes}m ${seconds}s`;
+  };
+
+  const [countdown, setCountdown] = useState(calculateCountdown());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCountdown(calculateCountdown());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <>
       <section className="bg-hope-beige py-14 px-8">
@@ -10,11 +38,11 @@ function HeroSection() {
           <h1 className="text-hope-darkcyan text-3xl font-black sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl">
             {CampTitle.CoreName} {CampTitle.Edition}
           </h1>
-          <p className="text-black text-center text-sm font-medium max-w-44 sm:text-base  lg:text-lg lg:max-w-lg xl:text-xl 2xl:text-2xl">
+          <p className="text-hope-blackcyan text-center text-sm  max-w-44 sm:text-base  lg:text-lg lg:max-w-lg xl:text-xl 2xl:text-2xl">
             Tabăra pe care nu vrei să o ratezi
           </p>
-          <p className="text-hope-lightcyan font-black text-3xl lg:text-5xl xl:text-7xl">
-            00:00:00
+          <p className="text-hope-lightcyan font-black text-2xl md:text-3xl lg:text-5xl xl:text-7xl">
+            {countdown}
           </p>
           <div className="pt-10">
             <FilledButton text="Înscrie-te" route={pages.register} />
@@ -25,4 +53,5 @@ function HeroSection() {
     </>
   );
 }
+
 export default HeroSection;
