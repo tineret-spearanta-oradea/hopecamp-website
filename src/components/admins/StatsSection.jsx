1c3174f21b7615077c72a3102cd851093510b219
Francis <65733799+MaxiFrancis@users.noreply.github.com>
2024-09-24 14:13:29 +0300

    Resolving class error
    chunk-XBHG3ID7.js?v=2b4ed4d9:521 Warning: Invalid DOM property `class`. Did you mean `className`?
        at div
        at button
        at div
        at div
        at StatsSection (http://localhost:5173/src/components/admins/StatsSection.jsx:36:25)
        at main
        at div
        at AdminDashboard (http://localhost:5173/src/components/admins/index.jsx?t=1727176108912:34:49)
        at RenderedRoute (http://localhost:5173/node_modules/.vite/deps/react-router-dom.js?v=15d8322f:3560:5)
        at Routes (http://localhost:5173/node_modules/.vite/deps/react-router-dom.js?v=15d8322f:3995:5)
        at Router (http://localhost:5173/node_modules/.vite/deps/react-router-dom.js?v=15d8322f:3938:15)
        at BrowserRouter (http://localhost:5173/node_modules/.vite/deps/react-router-dom.js?v=15d8322f:4678:5)
        at App
        at AuthProvider (http://localhost:5173/src/contexts/authContext/index.jsx:30:32)

--- src/components/admins/StatsSection.jsx
+++ src/components/admins/StatsSection.jsx
@@ -116,7 +116,7 @@ const StatsSection = ({ setSelectedSection }) => {
            "
           onClick={handleGoToUsers}
         >
-          <div class="absolute -inset-1 bg-gradient-to-r from-red-600 to-violet-600 rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
+          <div className="absolute -inset-1 bg-gradient-to-r from-red-600 to-violet-600 rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
           <div className="relative p-6 rounded-lg bg-white  ring-1 ring-gray-900/5">
             <h2 className="text-lg font-bold ">Participanti </h2>
             <div className="flex justify-between">
@@ -142,7 +142,7 @@ const StatsSection = ({ setSelectedSection }) => {
            "
           onClick={handleGoToMessages}
         >
-          <div class="absolute -inset-1 bg-gradient-to-r from-red-600 to-violet-600 rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
+          <div className="absolute -inset-1 bg-gradient-to-r from-red-600 to-violet-600 rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
           <div className="relative h-full p-6 rounded-lg bg-white ring-1 ring-gray-900/5">
             <h2 className="text-lg font-bold">Mesaje necitite </h2>
             <h2 className="text-md mb-2">din toate mesajele</h2>
