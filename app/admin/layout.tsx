5a89d09884175292d287a449f025b73cfb9d84e7
ghefraim <ghiurauefraim@gmail.com>
2025-01-13 19:52:41 +0200

    Fix some admins dashboard bugs

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,13 +1,18 @@
+"use client";
+
 import AdminProtected from "@/components/auth/AdminProtected";
 import { Sidebar } from "@/components/ui/sidebar";
 import { AdminNav } from "@/components/admin/nav";
 import { AuthProvider } from "@/contexts/auth-context";
+import { useIsMobile } from "@/hooks/use-mobile";
 
 export default function AdminLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const isMobile = useIsMobile();
+
   return (
     <AuthProvider>
       <AdminProtected>
@@ -15,7 +20,11 @@ export default function AdminLayout({
           <Sidebar>
             <AdminNav />
           </Sidebar>
-          <main className="flex-1 overflow-y-auto p-8">{children}</main>
+          <main
+            className={`flex-1 overflow-y-auto p-8 ${isMobile ? "pt-16" : ""}`}
+          >
+            {children}
+          </main>
         </div>
       </AdminProtected>
     </AuthProvider>
