11a4a6d2acf02426a6b38b3cafe6a23f406b90e6
DenisWithOneN <denis.ursut@emanuel.ro>
2025-05-13 10:36:22 +0300

    temporarly commented the grid layout till we have the schedule. Showing a placeholder message instead

--- components/ProgramSection.tsx
+++ components/ProgramSection.tsx
@@ -29,13 +29,29 @@ export default function ProgramSection() {
   return (
     <section className="bg-secondary py-16 sm:py-24">
       <div className="container mx-auto px-4 text-center">
-        <h3 className="text-sm uppercase text-foreground font-semibold tracking-wider mb-4">
+        <h3 className="font-jersey text-base uppercase text-foreground font-semibold tracking-wider mb-4">
           PROGRAM
         </h3>
-        <h2 className="font-lemon text-3xl sm:text-4xl md:text-5xl text-primary mb-10 sm:mb-16">
+        <h2 className="font-poppins font-bold uppercase text-3xl sm:text-4xl md:text-5xl text-primary mb-10 sm:mb-16">
           Ce vom face în tabără?
         </h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 sm:gap-8 justify-center">
+
+        <div className="max-w-xl mx-auto">
+          <p className="font-nunito mb-8">
+            În fiecare zi ne vom întâlni pentru a ne bucura împreună de
+            activități diverse, jocuri, închinare și multe altele. Dar, ne vom
+            asigura că vom avea și timp pentru noi înșine, pentru a ne relaxa și
+            a ne conecta cu cei din jurul nostru. Fie că alegi să te bucuri de
+            natură, să te implici în activități sau să te odihnești, fiecare zi
+            va fi plină de momente frumoase și oportunități de a te conecta cu
+            ceilalți.
+          </p>
+          <p className="font-poppins text-base font-bold text-primary mb-8">
+            Programul va fi anunțat în curând, dar ne dorim să ne bucurăm de
+            fiecare moment petrecut împreună.
+          </p>
+        </div>
+        {/* <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 sm:gap-8 justify-center">
           {eventDays.map((date, index) => (
             <Card
               key={index}
@@ -54,7 +70,7 @@ export default function ProgramSection() {
               </CardContent>
             </Card>
           ))}
-        </div>
+        </div> */}
       </div>
     </section>
   );
