4fdd37ddad467bc642f77dec0689292842edf0c8
ghefraim <ghiurauefraim@gmail.com>
2025-05-14 21:26:58 +0300

    try fix image preview 3

--- next.config.js
+++ next.config.js
@@ -23,6 +23,8 @@ const nextConfig = {
         port: "3000",
       },
     ],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     unoptimized: process.env.NODE_ENV === "production",
   },
   // other config options...
